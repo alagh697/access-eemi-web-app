6aae80b00db48d3b1ed2763cb4ab3233666341a2
alagh697 <gharbi.alaeddine@gmail.com>
2025-12-05 14:35:00 +0100

    actions ansible  commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           href="/landing"
           className="
             w-64 h-40 bg-white dark:bg-zinc-900 rounded-2xl shadow-md 
-            border border-zinc-200 dark:border-zinc-800
-            flex items-center justify-center
+             dark:border-zinc-800
+            flex items-center justify-center border border-red-500/30
             text-lg font-medium text-zinc-700 dark:text-zinc-200
             hover:shadow-xl hover:-translate-y-1 transition-all
           "
